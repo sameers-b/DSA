8127772e7f2720c732ac3e60035e0028f8f73dfd
Sameer Singh <78781539+sameers-b@users.noreply.github.com>
2024-05-14 12:02:51 +0530

    two-sum optimize solution

--- two-sum.js
+++ two-sum.js
@@ -11,3 +11,19 @@ var twoSum = function(nums, target) {
         }
     }
 };
+
+
+// other way
+
+function twoSum(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    // No solution found
+    return [];
+}
