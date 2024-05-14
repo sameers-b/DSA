4b3dd7b88c3d4521f77845c4f631b7650ca619dd
Sameer Singh <78781539+sameers-b@users.noreply.github.com>
2024-05-14 20:30:01 +0530

    container-with-most-water

---
+++ container-with-most-water.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+      
+      while (left < right) {
+          const minHeight = Math.min(height[left], height[right]);
+          const currentArea = minHeight * (right - left);
+          maxArea = Math.max(maxArea, currentArea);
+          
+          if (height[left] < height[right]) {
+              left++;
+          } else {
+              right--;
+          }
+      }
+      
+      return maxArea;
+  };
