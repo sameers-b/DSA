79387c30b88e117369b1df428a33264668222771
Sameer Singh <sameer@shadowcast.io>
2024-06-11 14:34:11 +0530

    dsa

---
+++ fibonacci/fibonacci-sequence.js
@@ -0,0 +1,20 @@
+/**
+ * Calculates the Fibonacci sequence up to the given number.
+ *
+ * @param {number} n - The number of elements in the Fibonacci sequence to calculate.
+ * @return {number[]} - The Fibonacci sequence up to the given number.
+ */
+var fibonacci = function (n) {
+  if (n <= 0) return [];
+  var result = [0];
+  if (n === 1) return result;
+  result.push(1);
+  for (var i = 2; i < n; i++) {
+    result[i] = result[i - 1] + result[i - 2];
+  }
+  return result;
+};
+console.log(fibonacci(1)); // [0]
+// console.log(fibonacci(2)); // [0,1]
+// console.log(fibonacci(3)); // [0,1,1]
+// console.log(fibonacci(7)); // [0,1,1,2,3,5,8]
---
+++ fibonacci/fibonacci-sequence.ts
@@ -0,0 +1,32 @@
+// Approach : Brute Force
+
+/**
+ * Calculates the Fibonacci sequence up to the given number.
+ *
+ * @param {number} n - The number of elements in the Fibonacci sequence to calculate.
+ * @return {number[]} - The Fibonacci sequence up to the given number.
+ */
+const fibonacci = (n: number): number[] => {
+  if (n <= 0) return [];
+
+  let result: number[] = [0];
+  if (n === 1) return result;
+
+  result.push(1);
+  for (let i: number = 2; i < n; i++) {
+    result[i] = result[i - 1] + result[i - 2];
+  }
+  return result;
+};
+
+console.log(fibonacci(1)); // [0]
+// console.log(fibonacci(2)); // [0,1]
+// console.log(fibonacci(3)); // [0,1,1]
+// console.log(fibonacci(7)); // [0,1,1,2,3,5,8]
+
+// Complexity
+// Time complexity: O(n)
+// Space complexity: O(n)
+// -------------------------------------------------------------
+
+// Approach : Recursion
