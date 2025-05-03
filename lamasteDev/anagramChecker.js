e0c309eb9a2331621f92667320c060fda92edf6f
Sameer Singh <78781539+sameers-b@users.noreply.github.com>
2025-05-03 15:02:15 +0530

    anagram

---
+++ lamasteDev/anagramChecker.js
@@ -0,0 +1,26 @@
+function isAnagram(str1, str2) {
+    // Your implementation
+    if (str1.length !== str2.length) return false;
+
+    if (str1.split("").sort().join("").toLowerCase() === str2.split("").sort().join("").toLowerCase()) return true
+    return false
+}
+
+module.exports = isAnagram
+
+
+
+// second solution
+
+var isAnagram = function(s, t) {
+    if (t.length !== s.length) return false;
+    const counts = {};
+    for (let c of s) {
+        counts[c] = (counts[c] || 0) + 1;
+    }
+    for (let c of t) {
+        if (!counts[c]) return false;
+        counts[c]--;
+    }
+    return true;
+};
