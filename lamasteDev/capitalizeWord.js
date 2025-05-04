70987c30292e5f6cb5a58cfada53eb7fcaf41cbc
Sameer Singh <78781539+sameers-b@users.noreply.github.com>
2025-05-04 09:53:50 +0530

    capitalize word

--- lamasteDev/capitalizeWord.js
+++ lamasteDev/capitalizeWord.js
@@ -1,8 +1,8 @@
 function capitalizeWords(sentence) {
     // Your implementation
     sentence = sentence.trim();
-    // sentence = sentence.trim(/\s+/); // slpit by one or more spaces
-    return sentence.split(" ").map(wrd=>wrd.charAt(0).toUpperCase()+wrd.slice(1).toLowerCase()).join(" ")
+//    sentence.split(/\s+/); // slpit by one or more spaces
+    return sentence.split(/\s+/).map(wrd=>wrd.charAt(0).toUpperCase()+wrd.slice(1).toLowerCase()).join(" ")
 }
 
 module.exports = capitalizeWords
