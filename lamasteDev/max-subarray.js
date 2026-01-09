6b4d4fe73e95ef5e608ea14ce63ef6a34096c227
Sameer Singh <78781539+sameers-b@users.noreply.github.com>
2026-01-09 15:48:06 +0530

    maxSubArray

---
+++ lamasteDev/max-subarray.js
@@ -0,0 +1,11 @@
+function maxSubArray(nums) {
+  let currSum = nums[0]
+  let maxSum = nums[0]
+
+  for (let i = 1; i < nums.length; i++) {
+    currSum = Math.max(nums[i], currSum + nums[i])
+    maxSum = Math.max(maxSum, currSum)
+  }
+
+  return maxSum
+}
