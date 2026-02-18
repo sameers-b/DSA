615bf3394ad62d4eaeb80f8f92283fe575b4a37b
Sameer Singh <78781539+sameers-b@users.noreply.github.com>
2026-02-18 15:46:34 +0530

    ts

---
+++ Theory/ts.js
@@ -0,0 +1,31 @@
+✅ Interface
+------
+   Defines object structure
+   Can extend
+   Supports declaration merging
+   Best for object models
+
+✅ Type
+-------
+   Creates custom type
+   Can define unions, intersections, primitives
+   More flexible than interface
+   No declaration merging
+
+✅ Difference
+-------
+   Interface → Mainly for objects
+   Type → For unions & complex types
+   Interface can merge, type cannot
+
+✅ Generics
+--------
+   Makes code reusable & type-safe
+   Type is decided later
+   Used in APIs, reusable components
+
+✅ Union
+--------
+   Allows multiple possible types
+
+   Used for states like loading | success | error
